d263c0dcb9aaa71ba4152fcc99d2e7d4aa7be314
Ankit Chaubey <chaubeyankit6102001@gmail.com>
2024-03-04 20:10:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from 'axios';
+function App(){
 
-function App() {
+  const [file, setFile] = useState(null);
+  const [progress, setProgress] = useState({started: false, pc: 0});
+  const [msg,setMsg] = useState(null);
+
+  function handleUpload(){
+    if (!file){
+      console.log("NO file Selected");
+      return;
+
+    }
+    const fd= new FormData();
+    fd.append('file', file);
+    setMsg("Uploading...");
+    setProgress(prevState => {
+      return {...prevState, started: true}
+    })
+    axios.post('http://httpbin.org/post', fd,{
+      onUploadProgress: (progressEvent) => { setProgress(prevState => {
+        return {...prevState, pc: progressEvent.progress * 100}
+        })   }, 
+      headers: {
+        "Custom-Header": "value",
+      }
+
+    })
+    .then(res => {
+      setMsg("Uploaded Successfully");
+      console.log(res.data)
+    })
+    .catch(err => {
+      console.log("Upload Failed");
+      console.log(err)});
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Uploading Files</h1>
+      <input onChange={(e) => {setFile(e.target.files[0])}} type="file" ></input>
+      <button onClick={ handleUpload }>Upload</button>
+      { progress.started && <progress max="100" value={progress.pc}></progress>}
+      { msg && <span>{msg}</span>}
     </div>
   );
 }
